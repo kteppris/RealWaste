799a24b5a8eb1164a8fd3e79d822c058e6f988ed
Teppris, Keno Alexander <keno.teppris@th-luebeck.de>
2024-02-25 14:32:43 +0100

    adjust devcontainer

--- .devcontainer/setup-devcontainer.sh
+++ .devcontainer/setup-devcontainer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Configure Git to recognize the repository directory as safe
+git config --global --add safe.directory /RealWaste
+
+# Install Python dependencies from requirements
+pip install -r /RealWaste/requirements.txt
+
 # Install Zsh and setup with desired themes and plugins
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.2/zsh-in-docker.sh)" -- \
     -t gnzh \
@@ -7,8 +13,3 @@ sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1
     -p https://github.com/zsh-users/zsh-completions \
     -p git 
 
-# Configure Git to recognize the repository directory as safe
-git config --global --add safe.directory /RealWaste
-
-# Install Python dependencies from requirements
-pip install -r /RealWaste/requirements-docker.txt
